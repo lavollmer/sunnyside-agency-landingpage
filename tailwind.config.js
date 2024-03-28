c6b18630289f1e65afa05107d6445cd60d92e7c7
Laura <laura.e.vollmer@gmail.com>
2024-03-28 15:35:25 -0500

    setting up tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
