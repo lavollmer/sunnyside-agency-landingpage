0a8236e5a4925057a623d1491bb46728fe5b90ea
Laura <laura.e.vollmer@gmail.com>
2024-03-31 16:44:41 -0500

    styling contact button

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ContactButton = () => {
   return (
     <div>
-        <button className='bg-white rounded-full'>
-            <p className='text-black font-bold text-sm'>CONTACT</p>
+        <button className='bg-white rounded-full '>
+            <p className='text-black font-bold text-xs'>CONTACT</p>
         </button>
     </div>
   )
