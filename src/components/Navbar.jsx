0db5eb582a0d84a60a7d222637d67afaf4560b10
Laura <laura.e.vollmer@gmail.com>
2024-04-01 15:07:29 -0500

    styling sidenav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,10 +25,10 @@ const Navbar = () => {
         <img src={Hamburger} alt="Hamburger icon for mobile menu" />
       </button>
       {isMenuOpen && (
-        <div className="flex flex-col items-center justify-center text-center fixed top-0 right-0 h-1/2 w-full md:w-1/2 p-10 bg-white z-50 ">
-          <Link to="/about" className="font-barlow font-600 text-md text-grayishblue">About</Link>
-          <Link to="/services" className="font-barlow font-600 text-md text-grayishblue">Services</Link>
-          <Link to="/projects" className="font-barlow font-600 text-md text-grayishblue">Projects</Link>
+        <div className="flex flex-col items-center justify-center text-center fixed top-0 right-0 h-1/2 w- md:w-1/2 p-10 bg-white z-50 ">
+          <Link to="/about" className="font-barlow font-600 text-md text-grayishblue mb-5">About</Link>
+          <Link to="/services" className="font-barlow font-600 text-md text-grayishblue mb-5">Services</Link>
+          <Link to="/projects" className="font-barlow font-600 text-md text-grayishblue mb-5">Projects</Link>
           <ContactButton color="bg-yellow" />
         </div>
       )}
