4cfdd536ab162f4c028d267c3434b4e064a2c7fa
Laura <laura.e.vollmer@gmail.com>
2024-04-01 10:59:32 -0500

    replicated first styling to other 2 testimonials - added correct copywrite

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -21,6 +21,26 @@ const Testimonials = () => {
           <p className="font-bold text-xs text-[#C7C6C4] ">Marketing Director</p>
           </div>
         </div>
+        <div className="flex flex-col items-center p-5 space-y-6">
+          <img src={Thomas} alt="Thomas image" className="rounded-full w-1/4 h1-/4 p-2" />
+          <h2 className="font-bold text-sm text-[#44444B] p-5 text-center">
+          Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.
+          </h2>
+          <div className="flex flex-col items-center">
+          <p className="font-bold text-lg text-[#252C34] pb-2">Thomas S.</p>
+          <p className="font-bold text-xs text-[#C7C6C4] ">Chief Operating Officer</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center p-5 space-y-6">
+          <img src={Jennie} alt="Jennie image" className="rounded-full w-1/4 h1-/4 p-2" />
+          <h2 className="font-bold text-sm text-[#44444B] p-5 text-center">
+          Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+          </h2>
+          <div className="flex flex-col items-center">
+          <p className="font-bold text-lg text-[#252C34] pb-2">Jennie F.</p>
+          <p className="font-bold text-xs text-[#C7C6C4] ">Business Owner</p>
+          </div>
+        </div>
       </div>
     </div>
   );
