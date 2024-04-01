20c7619d863904f5b0c9d8bb6619ae83bdc5c14d
Laura <laura.e.vollmer@gmail.com>
2024-04-01 11:36:19 -0500

    mobile responsive for audience and brand

--- src/components/Audience.jsx
+++ src/components/Audience.jsx
@@ -3,19 +3,16 @@ import { Link } from "react-router-dom";
 const Audience = () => {
   return (
     <div>
-      <div className="bg-[#FFFBF8] p-40 flex flex-col space-y-16">
+      <div className="bg-[#FFFBF8] p-20 md:p-40 flex flex-col items-center space-y-16">
         <h1 className="text-[#202C35]">Stand out to the right audience</h1>
         <p>
           Using a collaborative formula of designers, researchers,
           photographers, videographers, and copywriters, we’ll build and extend
           your brand in digital places.
         </p>
-        {/* add a 2px thick border to the bottom with color */}
-        <div className="border-b-10 border-[#F2E06B]">
-          <Link to="/learnmore">
-            <p className="font-bold text-sm text-[#333A3B]">LEARN MORE</p>
-          </Link>
-        </div>
+        <Link to="/learnmore" className="flex flex-row items-center">
+          <p className="font-bold text-sm text-[#333A3B]">LEARN MORE</p>
+        </Link>
       </div>
     </div>
   );
--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Brand = () => {
   return (
-    <div className="bg-[#FFFBF8] p-40 flex flex-col space-y-16">
-      <h1 className="text-[#202C35]">Transform your brand</h1>
+    <div className="bg-[#FFFBF8] p-20 md:p-40 flex flex-col items-center space-y-16">
+      <h1 className="flex flex-row items-center text-[#202C35]">Transform your brand</h1>
       <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-      <Link to="/learnmore" className="text-[#333A3B]">
+      <Link to="/learnmore" className="flex flex-row items-center text-[#333A3B] ">
         <p className="font-bold text-sm" >LEARN MORE</p>
       </Link>
     </div>
