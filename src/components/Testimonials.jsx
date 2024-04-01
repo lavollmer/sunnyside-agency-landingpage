3b02c71c86db805ce443006a340b36249f2eaab2
Laura <laura.e.vollmer@gmail.com>
2024-04-01 10:54:34 -0500

    styling font of Client Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -7,17 +7,17 @@ const Testimonials = () => {
   return (
     <div className="flex flex-col p-10">
       <div className="flex items-center justify-center">
-        <h1 className="text-[#C3C1C2] text-md font-bold pt-20">CLIENT TESTIMONIALS</h1>
+        <h1 style={{fontSize:'22px', letterSpacing:'2px'}} className="text-[#C3C1C2] font-bold pt-20 pb-10">CLIENT TESTIMONIALS</h1>
       </div>
       <div className="grid md:grid-cols-3">
         <div className="flex flex-col items-center p-5 space-y-6">
           <img src={Emily} alt="Emily image" className="rounded-full w-1/4 h1-/4 p-2" />
-          <h2 className="font-bold text-sm text-[#A2A2A2] p-5 text-center">
+          <h2 className="font-bold text-sm text-[#44444B] p-5 text-center">
             We put our trust in Sunnyside and they delivered, making sure our
             needs were met and deadlines were always hit.
           </h2>
           <div className="flex flex-col items-center">
-          <p className="font-bold text-lg text-[#44444B] pb-2">Emily R.</p>
+          <p className="font-bold text-lg text-[#252C34] pb-2">Emily R.</p>
           <p className="font-bold text-xs text-[#C7C6C4] ">Marketing Director</p>
           </div>
         </div>
