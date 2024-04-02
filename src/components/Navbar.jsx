d9c2569a92784b3b074ebf6b45aced6a63cd0a99
Laura <laura.e.vollmer@gmail.com>
2024-04-02 13:48:58 -0500

    adding triangle to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,27 +31,30 @@ const Navbar = () => {
         </button>
       </div>
       {isMenuOpen && (
-        <div className="flex flex-col items-center justify-center bg-white text-center fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-1/3 w-1/2 z-50">
-          <Link
-            to="/about"
-            className="font-barlow font-600 text-md text-grayishblue mb-5"
-          >
-            About
-          </Link>
-          <Link
-            to="/services"
-            className="font-barlow font-600 text-md text-grayishblue mb-5"
-          >
-            Services
-          </Link>
-          <Link
-            to="/projects"
-            className="font-barlow font-600 text-md text-grayishblue mb-5"
-          >
-            Projects
-          </Link>
-          <ContactButton color="bg-yellow" />
-        </div>
+        <>
+          <div className="flex flex-col items-center justify-center bg-white text-center fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-1/3 w-3/4 z-50">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-full w-0 h-0 border-8 border-transparent border-b-8 border-white"></div>
+            <Link
+              to="/about"
+              className="font-barlow font-600 text-md text-grayishblue mb-5"
+            >
+              About
+            </Link>
+            <Link
+              to="/services"
+              className="font-barlow font-600 text-md text-grayishblue mb-5"
+            >
+              Services
+            </Link>
+            <Link
+              to="/projects"
+              className="font-barlow font-600 text-md text-grayishblue mb-5"
+            >
+              Projects
+            </Link>
+            <ContactButton color="bg-yellow" />
+          </div>
+        </>
       )}
     </div>
   );
