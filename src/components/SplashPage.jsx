66564c300e82fb6e7fe099d605c2913d3133e95e
Laura <laura.e.vollmer@gmail.com>
2024-04-02 14:47:36 -0500

    finalizing for submission

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -3,18 +3,22 @@ import Orange from "../assets/images/desktop/image-header.jpg"; // Import the Or
 import Arrow from "../assets/icons/icon-arrow-down.svg"; // Import the Arrow icon
 
 const SplashPage = () => {
-    return (
-        <div className="relative">
-          {/* width of image to full length of the parent */}
-            <img src={Orange} alt="Orange Header photo" className="w-full" />
-            {/* position to the center of the div with the orange image */}
-            <div className="absolute top-1/3 left-1/2 md:top-1/3 md:left-1/2 md:transform -translate-x-1/2 -translate-y-1/2 text-center">
-                <h1 className="text-white font-fraunces font-bold font-900 text-3xl md:text-7xl tracking-wide md:text-7xl mb-5 md:mb-40">WE ARE CREATIVES</h1>
-                <img src={Arrow} alt="Arrow down icon" className="mx-auto w-4 h-16 md:w-10 md:h-40 md:mt-20" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative">
+      <img src={Orange} alt="Orange Header photo" className="w-full" />
+
+      <div className="absolute top-1/3 left-1/2 md:top-1/3 md:left-1/2 md:transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-white font-fraunces font-bold font-900 text-3xl md:text-7xl tracking-wide md:text-7xl mb-5 md:mb-40">
+          WE ARE CREATIVES
+        </h1>
+        <img
+          src={Arrow}
+          alt="Arrow down icon"
+          className="mx-auto w-4 h-16 md:w-10 md:h-40 md:mt-20"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SplashPage;
-
